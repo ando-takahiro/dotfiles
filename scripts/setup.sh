b2bff91d9cb2f3d347fd392ebef7d9ce0c664b90
ando <ando@ando-lubuntu.(none)>
2012-06-24 17:37:39 +0900

    remove log

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,14 +2,8 @@ DOTFILES=$(dirname $0)/..
 
 for f in $DOTFILES/files/* $DOTFILES/files/.*
 do
-    if echo $f | grep -e \/\.$ ; then
-        continue
-    fi
-    if echo $f | grep -e \/\.\.$ ; then
-        continue
-    fi
-    if echo $f | grep -e \~$ ; then
-        continue
-    fi
+    echo $f | awk "/\/\.$/{exit 1}" || continue
+    echo $f | awk "/\/\.\.$/{exit 1}" || continue
+    echo $f | awk "/~$/{exit 1}" || continue
     ln -s $f
 done
